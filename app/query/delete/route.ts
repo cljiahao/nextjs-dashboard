3d2f7061475b412a846160511f387d5d5d38b75e
cljiahao <cljiahao@hotmail.com>
2025-04-06 17:44:53 +0800

    updated finished course

--- app/query/delete/route.ts
+++ app/query/delete/route.ts
@@ -2,12 +2,9 @@ import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-async function listInvoices() {
+async function deleteTable() {
   const data = await sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    DROP TABLE customers
   `;
 
   return data;
@@ -19,7 +16,7 @@ export async function GET() {
   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
   // });
   try {
-    return Response.json(await listInvoices());
+    return Response.json(await deleteTable());
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
